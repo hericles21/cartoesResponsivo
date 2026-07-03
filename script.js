0117c817cecff4ba1dcaa84ae81b7db24d67a747
hericles21 <hericles.seixas@escola.pr.gov.br>
2026-07-03 12:47:21 +0000

    anime4

---
+++ script.js
@@ -0,0 +1,27 @@
+const botao1 = document.getElementById("botao");
+const botao2 = document.getElementById("botao");
+const botao3 = document.getElementById("botao");
+const botao4 = document.getElementById("botao");
+const botao5 = document.getElementById("botao");
+
+const caixa = document.getElementById("caixa-cartoes");
+
+botao1.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaaaaaaaaaaaa";)
+});
+
+botao2.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaaaaaaaaaaaa";)
+});
+
+botao3.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaaaaaaaaaaaa";)
+});
+
+botao4.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaaaaaaaaaaaa";)
+});
+
+botao5.addEventListener('click', () => {
+    alert("aaaaaaaaaaaaaaaaaaaaaaaa";)
+});
